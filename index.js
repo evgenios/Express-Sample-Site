f291cc121548d49099028e591b81fa7a4570eff5
evgenios <eugenioska@gmail.com>
2015-09-23 16:06:29 +0300

    Modified the code into a copypasta site

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.render('home');
+  res.render('home', { fortune: fortune.getFortune() });
 });
 
 app.get('/about', function(req, res) {
