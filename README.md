b72b04a5a8e7d17b133bae296e169fc85c19f270
evgenios <eugenioska@gmail.com>
2015-09-23 13:16:48 +0300

    Added templates and basic routes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Express-Sample-Site
 A sample site developed with Express.js
+
+The code was taken from the Web Development with Node and Express book, as
+a learning exercise.
---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const handlebars = require('express-handlebars')
+                  .create({defaultLayout: 'main'});
+const app = express();
+
+const fortunes = [
+  'Conquer your fears or they will conquer you.',
+  'Rivers need springs.',
+  'Do not fear what you don\'t know.',
+  'You will have a pleasant surprise.',
+  'Whenever possible, keep it simple.',
+];
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.get('/about', function(req, res) {
+  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', { fortune: randomFortune });
+});
+
+// 404 Response
+app.use(function(req, res) {
+  res.status(404);
+  res.render('404');
+});
+
+// 500 Response
+app.use(function(req, res) {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log(`Express started on http://localhost.com:${app.get('port')}
+    press Ctrl + C to exit`);
+});
+
+
